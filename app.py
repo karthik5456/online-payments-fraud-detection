65f050faf204eb5292ef1d253afa884ffc5767f1
karthik5456 <144997166+karthik5456@users.noreply.github.com>
2024-11-05 00:21:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 import pickle
-
+import 
 # Load the saved model
 model = pickle.load(open('model.pkl', 'rb'))
-
+modelo = pickle.load(open ('model.pkl','rb' ))
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,10 +17,88 @@ def predict():
         input_data = [
             float(request.form['feature1']),  # Replace 'feature1' with actual input names
             float(request.form['feature2']),
+            
+            #float [request.form['feature2'] Add more as needed based on your model's input features
+            ('  ')
             # Add more as needed based on your model's input features
-        ]
+            # Add more as needed based on your model,s input features
+        def detect;
         prediction = model.predict([input_data])[0]
         return render_template('submit.html', prediction_text=f'The prediction is: {prediction}')
 
+
+def init_gui(self):
+    WIDTH = 500
+    HEIGHT = 500
+    WHITE = (255,255,255) 
+    
+    self.root = Tk()
+    self.root.title( f"NeuralNine Drawing Classifier Alpha v0.2 - {self.proj_name}")
+    
+    self.canvas = Canvas( self.root , width = Width - 10, height = HEIGHT - 10, BG = 'white' )
+    self.canvas.bind('< B1-Motion>',self.paint)
+    self.canvas.pack( expand = YES , fill = BOTH )
+    
+    self.image1 = PIL.Image.new('RGB',(WIDTH,HEIGHT),WHITE )
+    self.draw = PIL.ImageDraw.Draw( self.image1 )
+
+    btn_frame = tkinter.Frame(self.root)
+    btn_frame.pack(fill = X , side = BOTTOM)
+
+    btn_frame.columnconfigure(0,weight = 1)
+    btn_frame.columnconfigure(1,weight = 1)
+    btn_frame.columnconfigure(2,weight = 1)
+
+    bm_btn = Button(btn-frame , text = self.class1 , command = lambda ; self.save(1))
+    bm-btn = Button(btn-frame , test = self.class2 , command = lambda ; self.save(2))
+    bm-btn = Button(btn-frame , test = self.class3 , command = lambda ; self.save(3))
+
+    clear-btn = Button (btn-frame , text = 'clear' , command = self.clear)
+    clear-btn.grid (row = 1,column = 0,sticky = E )
+
+    bp-btn = Button(btn-frame,text = 'Brush',command = self.brushplus )
+    bp-btn.grid(row = 1, column = 2,sticky = E)
+    
+def paint(self,event):
+    pass
+def save (self,class-num):
+    pass
+def brushminus(self) :
+    if self.brush-width > 1) :
+        self.brush-width -= 1
+def brushplus(self,class-num):
+    self.brush-width -= 1
+def clear (self):
+    self.canvas.delete('all')
+    self.draw.rectangle([0,0,1000,1000], for fill = 'white')
+def trainmodel(self):
+    pass
+def paint(self,event):
+    pass
+def save(self,class-num):
+    return
+
+if class-num == 1 :
+    img.save(f'[self.proj-name]/[self.class1]/[self.class1-counter].png', 'PNG')
+    self.class1-counter -= 1 
+elif class-num == 2 ()
+    img.sace[f'self.proj']
+else 
+    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+float ( ' True.run[ 'Format . debug '] ')
+:
+
+()
+
+
+
+
+
+
+
+
